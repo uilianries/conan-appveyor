6f4356442fd1d76e571d7be4699ff8a353786439
Uilian Ries <uilianries@gmail.com>
2019-06-05 17:07:09 -0300

    Build project
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

---
+++ conanfile.py
@@ -0,0 +1,27 @@
+from conans import ConanFile, CMake, tools
+
+
+class FooConan(ConanFile):
+    name = "foo"
+    version = "0.1"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = {"shared": False}
+    generators = "cmake"
+    exports_sources = "src/*"
+
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="src")
+        return cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        cmake = self._configure_cmake()
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
