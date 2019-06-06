6c09ef8d15b65389a7056098637987a60c9fe35a
Uilian Ries <uilianries@gmail.com>
2019-06-06 11:21:24 -0300

    Add new test
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

---
+++ conan_authenticate.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from conans.client.conan_api import Conan
+
+if __name__ == "__main__":
+    password = os.getenv("CONAN_PASSWORD", None)
+    if not password:
+        raise Exception("Password can not be empty.")
+
+    remote_name = "bintray"
+    remote_url = os.getenv("CONAN_REMOTE", "https://api.bintray.com/conan/uilianries/conan")
+    name = os.getenv("CONAN_USERNAME", "uilianries")
+
+    conan_api, _, _ = Conan.factory()
+    conan_api.remote_add(remote_name, remote_url)
+    conan_api.authenticate(name, password, remote_name)
